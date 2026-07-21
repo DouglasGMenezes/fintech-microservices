26444c0afeccfd12d3a841bb30dea84bd4bf1514
Douglas Menezes <goulartdouglas4@gmail.com>
2026-07-21 19:08:28 -0300

    feat(mscartoes): finished implementation CardIssuanceSubscriber/receiveIssuanceRequest()

--- mscartoes/src/main/java/br/com/dgm/mscartoes/infra/repository/infra/mqueue/CardIssuanceSubscriber.java
+++ mscartoes/src/main/java/br/com/dgm/mscartoes/infra/repository/infra/mqueue/CardIssuanceSubscriber.java
@@ -1,15 +1,40 @@
 package br.com.dgm.mscartoes.infra.repository.infra.mqueue;
 
+import br.com.dgm.mscartoes.domain.model.Card;
+import br.com.dgm.mscartoes.domain.model.CardCustomer;
+import br.com.dgm.mscartoes.domain.model.CardIssuanceRequestData;
+import br.com.dgm.mscartoes.infra.repository.CardCustomerRepository;
+import br.com.dgm.mscartoes.infra.repository.CardRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 
 @Component
+@RequiredArgsConstructor
 public class CardIssuanceSubscriber {
+
+    private final CardRepository cardRepository;
+    private final CardCustomerRepository cardCustomerRepository;
 
     @RabbitListener(queues="${mq.queues.card-issuance}")
     public void receiveIssuanceRequest(@Payload String payload) {
-        System.out.println(payload);
+        try {
+            var mapper = new ObjectMapper();
+            CardIssuanceRequestData data = mapper.readValue(payload, CardIssuanceRequestData.class);
+
+            Card card = cardRepository.findById(data.getCardId()).orElseThrow();
+
+            CardCustomer cardCustomer = new CardCustomer();
+            cardCustomer.setCard(card);
+            cardCustomer.setTaxId(data.getTaxId());
+            cardCustomer.setApprovedLimit(data.getAvailableLimit());
+            cardCustomerRepository.save(cardCustomer);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
